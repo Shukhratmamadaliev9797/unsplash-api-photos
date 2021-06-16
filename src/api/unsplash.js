93a1b4b2c30d900bcc67ef89eea19e3d4df5a481
Shukhrat <shukhrat@shukhrats-mbp.home>
2021-06-16 14:46:04 +0100

    test key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: `Client-ID ${process.env.REACT_APP_KEY}`,
+    Authorization: `Client-ID ${
+      process.env.REACT_APP_KEY || "A62ckDfO-_LHZA-6SHb9fFo-8a4M80NnkFMqpipjsGg"
+    }`,
   },
 });
